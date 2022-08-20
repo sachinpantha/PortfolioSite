94024fb553392dc60baefd0f4021dc6c14fcfe4f
sarad096 <saradgyawali096@gmail.com>
2022-08-20 19:59:22 +0545

    Added Pacman Loader

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./loader.css";
+const Loader = () => {
+  return (
+    <div>
+      <div className="loader">
+        <div className="circles">
+          <span className="one"></span>
+          <span className="two"></span>
+          <span className="three"></span>
+        </div>
+        <div className="pacman">
+          <span className="top"></span>
+          <span className="bottom"></span>
+          <span className="left"></span>
+          <div className="eye"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
